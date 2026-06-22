1d44c9364d080dd07d39e6361bfe9d26d8bf4942
mohnoor94 <moh.noor94@gmail.com>
2026-06-22 17:46:58 +0300

    fix: remove trailing period from AURA branding in OpenGraph image

--- frontend/src/app/opengraph-image.tsx
+++ frontend/src/app/opengraph-image.tsx
@@ -90,7 +90,7 @@ export default async function Image() {
                 color: 'transparent',
               }}
             >
-              AURA.
+              AURA
             </div>
           </div>
 
